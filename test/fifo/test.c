3078eaed4074290f809cdb0977a32b304f6d6136
Taylor Stroobosscher <stroobos@usc.edu>
2019-02-13 21:46:49 -0800

    added test case init

--- test/fifo/test.c
+++ test/fifo/test.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "../../fifo.h"
 #include "../../utils.h"
 
+int test_fifo_buf(struct fifo_t *fifo, char *exp, int size) {
+
+	char *ret = malloc(size);
+
+	free(ret);
+	return 0;
+}
+
 int main() {
 
-	struct fifo_t *fifo = malloc(sizeof(struct fifo_t));
-
-	fifo_init(fifo);
+	struct fifo_t *fifo_ptr = malloc(sizeof(struct fifo_t));
 
 	char dat[] = {
 		0x00, 0x01, 0x02, 0x03,
@@ -17,13 +24,29 @@ int main() {
 		0x0C, 0x0D, 0x0E, 0x0F,
 	};
 
-	if(fifo_writen(fifo, dat, ARRAY_SIZE(dat)) < 0) {
+	char *ret = malloc(sizeof(dat));
+
+	fifo_init(fifo_ptr);
+
+	/*************************************************************************\
+	 *
+	 *	head and tail should be 0, status should be empty
+	 *
+	\*************************************************************************/
+
+	if(fifo_ptr->fifo_status == FIFO_EMPTY)
+		printf("Test 1: Success, Fifo initialized empty\n");
+	else
+		printf("Test 1: failure, Fifo status: %d\n", fifo->fifo_status);
+
+	if(fifo_writen(fifo_ptr, dat, ARRAY_SIZE(dat)) < 0) {
 		printf("fifo_writen: failure\n");
 		return -1;
 	}
 
-	fifo_dump_buf(fifo);
+	fifo_dump_buf(fifo_ptr);
 
-	free(fifo);
+	free(fifo_ptr);
+	free(ret);
 	return 0;
 }
