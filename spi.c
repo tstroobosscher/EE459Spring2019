f63b89589e40d3427ecdc0713bba3f4f19c346d9
Taylor Stroobosscher <stroobos@usc.edu>
2019-02-21 13:44:34 -0800

    added error logging in debug mode for SPI transfers

--- spi.c
+++ spi.c
@@ -72,7 +72,9 @@ int8_t spi_device_disable(uint8_t dev) {
 uint8_t spi_write_char(uint8_t ch) {
     SPDR = ch;
     while(!(SPSR & (1 << SPIF))) {
-    	uart_write_str("trace\n\r");
+#ifdef DEBUG_328
+    	uart_write_str("spi: transfer not complete\n\r");
+#endif
     }
     return SPDR;
 }
