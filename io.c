d90bf23b872340fe4d61ea01c1126aa15e603283
Taylor Stroobosscher <stroobos@usc.edu>
2019-02-21 14:08:32 -0800

    added io layer

---
+++ io.c
@@ -0,0 +1,13 @@
+/*
+ *	USC EE459 Spring 2019 Team 17 - Input/Output Abstraction Layer
+ */
+
+#include "io.h"
+
+/*
+ *	here we need to implement the POSIX file io functions:
+ *		open()
+ *		read()
+ *		write()
+ *		seek()
+ */
---
+++ io.h
@@ -0,0 +1,13 @@
+/*
+ *	USC EE459 Spring 2019 Team 17 - IO Header
+ */
+
+#ifndef IO_H
+#define IO_H
+
+/*
+ *	io buffer structures
+ */
+
+
+#endif
