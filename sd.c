93494c9b823f58a2f3a225c95ee961c7d994bfde
Taylor Stroobosscher <stroobos@usc.edu>
2019-02-22 18:25:55 -0800

    need timer functions

--- sd.c
+++ sd.c
@@ -10,6 +10,28 @@
 #include "pins.h"
 #include "utils.h"
 
+static void sd_command(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t bytes, 
+	uint8_t *buf) {
+	/*
+	 *	expecting a response of bytes length
+	 *	buf must be at least bytes long
+	 *	
+	 *	SPI SD does not use crc, just default 0xFF once SPI is init'd
+	 */
+
+    spi_write_char(cmd);
+    spi_write_char(arg>>24);
+    spi_write_char(arg>>16);
+    spi_write_char(arg>>8);
+    spi_write_char(arg);
+    spi_write_char(crc);
+                
+    for(uint8_t i = 0; i < bytes; i++)
+        buf[i] = spi_write_char(0xFF);
+                
+    spi_device_disable(SPI_SD_CARD);
+}
+
 int8_t initialize_sd() {
 	uint8_t buf[CMD_RESP_BYTES];
 
@@ -50,50 +72,10 @@ int8_t initialize_sd() {
 		/* too late */
 		if(timeout())
 			return -1;
+
+	/* check if 0x01 is in the ret array */
 	} while(!byte_in_arr(0x01, buf, CMD_RESP_BYTES));
 
-	/* need to check if 0x01 is in the ret array */
-
-	uint8_t j = 0;
-
-cmd1:
-	sd_command(CMD1, bind_args(NOARG, NOARG, NOARG, NOARG), NOCRC, 8, buf);
-
-	if(byte_in_arr(0x00, buf, 8) < 0) {
-		uart_write_str("sd: CMD1 returned incorrectly, retrying\n\r");
-		dump_nbytes(buf, CMD_RESP_BYTES);
-		j++;
-		if(j==10)
-			return;
-		goto cmd1;
-	} else
-		uart_write_str("sd: CMD1 returned correctly\n\r");
-}
-
-void sd_command(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t bytes, 
-	uint8_t *buf) {
-	/*
-	 *	expecting a response of bytes length
-	 *	buf must be at least bytes long
-	 *	
-	 *	SPI SD does not use crc, just default 0xFF once SPI is init'd
-	 */
-
-    spi_write_char(cmd);
-    spi_write_char(arg>>24);
-    spi_write_char(arg>>16);
-    spi_write_char(arg>>8);
-    spi_write_char(arg);
-    spi_write_char(crc);
-                
-    for(uint8_t i = 0; i < bytes; i++)
-        buf[i] = spi_write_char(0xFF);
-                
-    spi_device_disable(SPI_SD_CARD);
-}
-
-int8_t sd_set_block_len() {
-	uint8_t buf[8];
-	sd_command(CMD16, bind_args(0x00, 0x00, 0x02, 0x00), NOCRC, 8, buf);
+	return 0;
 
 }
--- time.h
+++ time.h
@@ -1,3 +1,10 @@
 /*
  *	USC EE459 Spring 2019 Team 17 - Timer header
  */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+
+
+#endif
