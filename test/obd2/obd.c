4469270580a975dd05b7e7dd6e1502f3236f8f5e
Taylor Stroobosscher <stroobos@usc.edu>
2019-02-26 19:06:27 -0800

    updated obd test code

--- test/obd2/obd.c
+++ test/obd2/obd.c
@@ -9,57 +9,57 @@
 
 int set_tty_attr(int fd, int speed, int parity)
 {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
 
-        if (tcgetattr (fd, &tty) != 0) {
-                printf("error %d from tcgetattr", errno);
-                return -1;
-        }
+    if (tcgetattr (fd, &tty) != 0) {
+            printf("error %d from tcgetattr", errno);
+            return -1;
+    }
 
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
+    cfsetospeed (&tty, speed);
+    cfsetispeed (&tty, speed);
 
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK;         // disable break processing
+    tty.c_lflag = 0;                // no signaling chars, no echo,
+                                    // no canonical processing
+    tty.c_oflag = 0;                // no remapping, no delays
+    tty.c_cc[VMIN]  = 0;            // read doesn't block
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                    // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+    tty.c_cflag |= parity;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0) {
-                printf("error %d from tcsetattr", errno);
-                return -1;
-        }
-        return 0;
+    if (tcsetattr (fd, TCSANOW, &tty) != 0) {
+            printf("error %d from tcsetattr", errno);
+            return -1;
+    }
+    return 0;
 }
 
 void set_tty_blocking (int fd, int should_block)
 {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0) {
-                printf("error %d from tggetattr", errno);
-                return;
-        }
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
+    if (tcgetattr (fd, &tty) != 0) {
+            printf("error %d from tggetattr", errno);
+            return;
+    }
 
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+    tty.c_cc[VMIN]  = should_block ? 1 : 0;
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-                printf("error %d setting term attributes", errno);
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+            printf("error %d setting term attributes", errno);
 }
 
 int main(int argc, char * argv[]) {
@@ -123,7 +123,37 @@ int main(int argc, char * argv[]) {
 			break;
 	}
 
-	printf("\n\nret: %s\n", ret);
+	printf("\n\n");
+
+	/* if "OK" not in the return string, error */
+	if(!strstr(ret, "OK"))
+		printf("%s: ERROR: unable to initialize ELM327\n", pname);
+	else
+		printf("%s: ELM327 initialized\n", pname);
+
+	write (fd, "0100\r\n", 5);
+
+	i = 0;
+
+	for(i = 0; i < 50; i++){
+		read(fd, &buf, sizeof(buf));
+		write(STDOUT_FILENO, &buf, sizeof(buf));
+
+		ret[i] = buf;
+
+		if(buf == '>')
+			break;
+	}
+
+	printf("%s: ret: %s\n", pname, ret);
+
+	/* read battery voltage */
+	//ATRV
+	
+	/* search for protocol */
+	//ATSP0
+
+
 
 	return 0;
 }
