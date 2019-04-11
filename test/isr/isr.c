d98e254d2f91dc9e087f9c55ea98927b5462899d
Taylor Stroobosscher <stroobos@usc.edu>
2019-04-10 17:37:12 -0700

    holy shit have to read UDRX

--- test/isr/isr.c
+++ test/isr/isr.c
@@ -4,32 +4,42 @@
  
 #define FOSC 16000000
 
-#define BUAD_UART_0 38400
+#define BUAD_UART_0 9600
 
 #define MYUBRR(x) ((FOSC / 16 / x) - 1)
 
+volatile int index = 0;
+
+void uart_write_char(char data) {
+  while (!(UCSR0A & (1 << UDRE0))) {
+  }
+  UDR0 = data;
+}
 
 int main() {
   DDRB |= (1 << 7);
 
-  sei();
+  sei();           
   
   UBRR0=MYUBRR(BUAD_UART_0);
  
   UCSR0B|=(1<<RXCIE0)|(1<<TXEN0)|(1<<RXEN0);
  
-  UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00); 
+  UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00);
 
   while(1) {
     PORTB |= (1 << 7);
     _delay_ms(1000);
     PORTB &= ~(1 << 7);
     _delay_ms(1000);
- }
+    uart_write_char(index + 48);
+  }
 
- return 0;
+  return 0;
 }
 
 ISR(USART0_RX_vect) {
-
+  index++;
+  char x = UDR0;
 }
+
